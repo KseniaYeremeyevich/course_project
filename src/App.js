e9c854315b5405f11d97fecd1ebaba4501b49a5e
Ksenia <k.yeremeyevich@gmail.com>
2021-11-02 18:31:51 +0300

    add src

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="time">
+        <p className="text_time"> 
+          УТРО
+        </p>
+      </div>
+    
+      <div className="time">
+        <p className="text_time"> 
+          ДЕНЬ
+        </p>  
+      </div>
+    
+      <div className="time">
+        <p className="text_time"> 
+          ВЕЧЕР
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
