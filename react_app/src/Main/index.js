b7374b94a57df8e2de5e15d750251140debaffe7
Ksenia <k.yeremeyevich@gmail.com>
2021-11-11 23:30:19 +0300

    Update src

---
+++ react_app/src/Main/index.js
@@ -0,0 +1,29 @@
+import '../App.css';
+
+const Main = () => {
+  return (
+    
+    <div className='App'>
+      <div className='time'>
+        <p className='text_time'> 
+          УТРО
+        </p>
+      </div>
+    
+      <div className='time'>
+        <p className='text_time'> 
+          ДЕНЬ
+        </p>  
+      </div>
+    
+      <div className='time'>
+        <p className='text_time'> 
+          ВЕЧЕР
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Main;
+
